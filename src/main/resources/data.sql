02865ae1aee95b425fd84ece108bd2ad352e2a36
Scheila Stihler <sstihler@gmail.com>
2024-04-19 21:15:00 -0300

    Realizada exclusão de main devido a conflitos que não puderam ser corrigidos.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,4 @@ on conflict (id) do nothing;
 insert into usuario (login, senha, id_papel)
 values ('admin', '$2a$08$2dFreuQvrRC.uM4yv/Y46OK13VpWUBUzODfruvFhNfvjB0pWZGhSW', 1)
 on conflict (login) do nothing;
+
